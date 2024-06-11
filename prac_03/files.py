e8c58154c7706139e28aa6ff87f5419dec33a362
Dream <shuhan.li@my.jcu.edu.au>
2024-06-11 23:21:26 +0800

    Finish files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,3 +1,10 @@
+"""
+Write code that asks the user for their name,
+then opens a file called and writes that name to it.
+Use and for this question.
+"""
+
+
 def question_1():
     file = open("name.txt", "w")
     name = input("Name: ")
@@ -5,11 +12,30 @@ def question_1():
     file.close()
 
 
+"""
+In the same file, but as if it were a separate program,
+write code that opens "name.txt" and reads the name (as above) then prints (note the exact output),
+(or whatever the name is in the file). Do not simply print the user's input variable!
+Use and for this question.
+"""
+
+
 def question_2():
     file = open("name.txt", "r")
     name = file.read().strip()
     print(f"Hi {name}!")
     file.close()
+
+
+"""
+Create a text file called and save it in your prac directory. 
+Put the following three numbers on separate lines in the file and save it:
+17
+42
+400
+Write code that opens , reads only the first two numbers, adds them together then prints the result,
+which should be... 59. Use instead of and for this question.
+"""
 
 
 def question_3():
@@ -20,6 +46,13 @@ def question_3():
             total += int(number)
         print(total)
     file.close()
+
+
+"""
+Now write a fourth block of code that prints the total for all lines in .
+This should work for a file with any number of numbers.
+Use instead of and for this question.
+"""
 
 
 def question_4():
